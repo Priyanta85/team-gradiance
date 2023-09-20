dd3b1d5444f91ec26fa6892dcf0eb26d0636e885
Priyanta Islam <pislam@talhackiverse.onmicrosoft.com>
2023-09-20 16:29:59 +1000

    setting up repo

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { getTokenOrRefresh } from './token_util';
+import './custom.css'
+import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayText: 'INITIALIZED: ready to test speech...'
+        }
+    }
+    
+    async componentDidMount() {
+        // check for valid speech key/region
+        const tokenRes = await getTokenOrRefresh();
+        if (tokenRes.authToken === null) {
+            this.setState({
+                displayText: 'FATAL_ERROR: ' + tokenRes.error
+            });
+        }
+    }
+
+    async sttFromMic() {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+        
+        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        this.setState({
+            displayText: 'speak into your microphone...'
+        });
+
+        recognizer.recognizeOnceAsync(result => {
+            let displayText;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                displayText = `RECOGNIZED: Text=${result.text}`
+            } else {
+                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+
+            this.setState({
+                displayText: displayText
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Container className="app-container">
+                <h1 className="display-4 mb-3">Speech sample app</h1>
+
+                <div className="row main-container">
+                    <div className="col-6">
+                        <button onClick={() => this.sttFromMic()}>Start</button>
+                    </div>
+                    <div className="col-6 output-display rounded">
+                        <code>{this.state.displayText}</code>
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+}
